274f286423b729530bd399d74a3e0e3383cb4dd5
Mauricio Vazquez <maurovz@gmail.com>
2022-10-09 13:17:01 +0200

    Add Crypto.com wallet

--- Sources/Glaip/API/Domain/WalletType.swift
+++ Sources/Glaip/API/Domain/WalletType.swift
@@ -9,4 +9,5 @@ public enum WalletType: String, Equatable {
   case MetaMask = "https://metamask.app.link/wc?uri="
   case Rainbow = "https://rnbwapp.com/wc?uri="
   case TrustWallet = "https://link.trustwallet.com/wc?uri="
+  case CryptoCom = "https://wallet.crypto.com"
 }
